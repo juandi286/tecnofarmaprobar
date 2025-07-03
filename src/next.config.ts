50a57a85d1370d006b2936df5199202b4c5ba166
JuanDi <juandiego28062006@gmail.com>
2025-07-03 18:19:17 +0000

    Mira bro: $ npm run dev
    > nextn@0.1.0 dev
    > next dev --turbopack -p 90

---
+++ src/next.config.ts
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
