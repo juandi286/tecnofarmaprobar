e8afe7d38a055477b5349ffd8b0451592f6334d7
juandi286 <juandiego28062006@gmail.com>
2025-07-28 13:50:31 -0500

    Se elimino una carpeta innecesaria

--- TecnoFarma/env.js
+++
@@ -1,7 +0,0 @@
-// Carga las variables de entorno desde el archivo .env
-// Este es el método recomendado para asegurar que estén disponibles en next.config.js y en todo el proyecto.
-const { loadEnvConfig } = require('@next/env');
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
-
-module.exports = {};
